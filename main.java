6d16aae66ec3a322ae26e0bc679439d34300ccb5
samthegitguy <55744004+samthegitguy@users.noreply.github.com>
2020-08-24 13:18:58 +1000

    add jar

--- main.java
+++ main.java
@@ -12,7 +12,7 @@ public class main {
      */
     private static void createAndShowGUI() {
         //Create and set up the window.
-        JFrame frame = new JFrame("Time for your break. Time for your break. Time for your break. Very obnoxious");
+        JFrame frame = new JFrame("Time for your break. Time for your break. Time for your break. Time for your break. ");
  
         JLabel emptyLabel = new JLabel("");
         emptyLabel.setPreferredSize(new Dimension(1650, 1080));
