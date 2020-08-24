fc26a6c8221c5ee1928082e1922dc9c4b8e4027c
samthegitguy <55744004+samthegitguy@users.noreply.github.com>
2020-08-24 13:06:27 +1000

    Added sleep and for loop

--- main.java
+++ main.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.lang.*;
  
 /* FrameDemo.java requires no other files. */
 public class main {
@@ -11,8 +12,7 @@ public class main {
      */
     private static void createAndShowGUI() {
         //Create and set up the window.
-        JFrame frame = new JFrame("Time up! ");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame frame = new JFrame("Time for your break. Time for your break. Time for your break. Very obnoxious");
  
         JLabel emptyLabel = new JLabel("");
         emptyLabel.setPreferredSize(new Dimension(1650, 1080));
@@ -23,14 +23,17 @@ public class main {
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
         frame.setVisible(true);
     }
- 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
+        for (int i = 0; i < 10000; i++) { // i < arbritary 
+            javax.swing.SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    createAndShowGUI();
+                }
+            });
+            Thread.sleep(600000);
+             
+          }
     }
 }
