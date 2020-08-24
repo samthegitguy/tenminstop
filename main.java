1ee854d545bc93309e265850462014601a0d154b
samthegitguy <55744004+samthegitguy@users.noreply.github.com>
2020-08-24 12:46:51 +1000

    Blank box

--- main.java
+++ main.java
@@ -1,6 +1,36 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+ 
+/* FrameDemo.java requires no other files. */
 public class main {
-    public static void main(String[] args) {
-       // Prints "Hello, World" in the terminal window.
-       System.out.println("Hello, World");
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("Time up! ");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        JLabel emptyLabel = new JLabel("");
+        emptyLabel.setPreferredSize(new Dimension(1650, 1080));
+        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+ 
+        //Display the window.
+        frame.pack();
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        frame.setVisible(true);
     }
- }
+ 
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
